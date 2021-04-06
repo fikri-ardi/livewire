1c4159ee4548e397c2d16b1368e6d5de0e33086f
fikri-dev <muhammadfikriardi@gmail.com>
2021-04-06 13:14:12 +0700

    styling login form

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,24 +1,23 @@
-<form wire:submit.prevent="register">
-    <div>
-        <label for="email">E-mail</label>
-        <input wire:keyup="validate_email" wire:model="email" type="email" id="email" name="email">
-        @error('email') <span>{{ $message }}</span> @enderror
-    </div>
+<div class="container">
+    <form wire:submit.prevent="register">
+        <p>Create an account</p>
 
-    <div>
-        <label for="password">Password</label>
-        <input wire:keyup="validate_password" wire:model="password" type="text" id="password" name="password">
-        @error('password') <span>{{ $message }}</span> @enderror
-    </div>
+        <div class="form-group email">
+            <input wire:keyup="validate_email" wire:model="email" type="email" id="email" name="email" placeholder="email">
+            @error('email') <div class="validation-errors">{{ $message }}</div> @enderror
+        </div>
 
-    <div>
-        <label for="passwordConfirmation">Password Confirmation</label>
-        <input wire:keyup="validate_password_confirm" wire:model="passwordConfirmation" type="text" id="passwordConfirmation"
-            name="passwordConfirmation">
-        @error('passwordConfirmation') <span>{{ $message }}</span> @enderror
-    </div>
+        <div class="form-group">
+            <input wire:keyup="validate_password" wire:model="password" type="text" id="password" name="password" placeholder="password">
+            @error('password') <div class="validation-errors">{{ $message }}</div> @enderror
+        </div>
 
-    <div>
-        <button type="submit">Register</button>
-    </div>
-</form>
+        <div class="form-group">
+            <input wire:keyup="validate_password_confirm" wire:model="passwordConfirmation" type="text" id="passwordConfirmation"
+                name="passwordConfirmation" placeholder="password confirmation">
+            @error('passwordConfirmation') <div class="validation-errors">{{ $message }}</div> @enderror
+        </div>
+
+        <button class="register-btn" type="submit">REGISTER</button>
+    </form>
+</div>
